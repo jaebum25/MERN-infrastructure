dd48efd67662e4dbe02c7f19b411a344c5194a2b
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2023-02-28 02:16:09 -0500

    setup front and back end folders

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <main className="App">
       <h1>hello everyone!</h1>
-    </div>
+    </main>
   );
 }
